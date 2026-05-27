3c8c7f6ee7c732f2dabfc680b01ad2ca495c0abf
francisco1020111-hue <francisco1020111@gmail.com>
2026-05-27 11:41:21 -0300

    Add files via upload

---
+++ try.js
@@ -0,0 +1,13 @@
+function meuPao() {
+
+    let opcao =
+    document.getElementById("opcao").value;
+
+    switch (opcao) {
+
+        case"1":
+
+        document.getElementById("resultadoSwitch").innerText =
+        "Pedido a fazer"
+    }
+}
